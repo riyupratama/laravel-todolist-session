6466332b625d7b83e3b19b206c6019e06771982b
Riyu Rahmat Pratama <riyurahmatpratama6@gmail.com>
2023-10-26 11:14:23 +0700

    slightly changed the appearance of the todolist blade

--- resources/views/todolist/todolist.blade.php
+++ resources/views/todolist/todolist.blade.php
@@ -27,8 +27,8 @@
     <div class="row align-items-center g-lg-5 py-5">
         <div class="col-lg-7 text-center text-lg-start">
             <h1 class="display-4 fw-bold lh-1 mb-3">Todolist</h1>
-            <p class="col-lg-10 fs-4">by <a target="_blank" href="https://www.programmerzamannow.com/">Programmer Zaman
-                    Now</a></p>
+            <span class="col-lg-10 fs-4 text-primary">Riyu Todolist data with session</span><br>
+            <span class="text-muted mt-0">Only use session to keep data (no use Database)</span>
         </div>
         <div class="col-md-10 mx-auto col-lg-5">
             <form class="p-4 p-md-5 border rounded-3 bg-light" method="post" action="/todolist">
